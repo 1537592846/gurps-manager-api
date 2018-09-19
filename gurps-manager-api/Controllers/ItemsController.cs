b93fab1eb371e982653e6daf1c424f240bb8e853
André Soares <770688@tce.ro.gov.br>
2018-09-19 08:51:20 -0400

    Item data

---
+++ gurps-manager-api/Controllers/ItemsController.cs
@@ -0,0 +1,34 @@
+﻿using gurps_manager_library.DataAccess;
+using gurps_manager_library.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace gurps_manager_api.Controllers
+{
+    [Route("api/[controller]")]
+    public class ItemsController : Controller
+    {
+        [HttpGet("get")]
+        public string Get()
+        {
+            return new ItemDataAccess().ReturnAllData<Item>();
+        }
+
+        [HttpGet("get/{id}")]
+        public string Get(int id)
+        {
+            return new ItemDataAccess().Find(id);
+        }
+
+        [HttpGet("insert")]
+        public void Insert()
+        {
+            Item.InsertItems();
+        }
+
+        [HttpGet("delete")]
+        public void Delete()
+        {
+            new ItemDataAccess().DeleteAll<Item>();
+        }
+    }
+}
---
+++ gurps-manager-api/Models/Item.cs
@@ -0,0 +1,85 @@
+﻿using gurps_manager_library.DataAccess;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using System.Collections.Generic;
+
+namespace gurps_manager_library.Models
+{
+    public class Item
+    {
+        public ObjectId DBId;
+
+        [BsonElement("Id")]
+        public int Id;
+
+        [BsonElement("Name")]
+        public string Name;
+
+        [BsonElement("NT")]
+        public int NT;
+
+        [BsonElement("Description")]
+        public string Description;
+
+        [BsonElement("Cost")]
+        public int Cost;
+
+        [BsonElement("Weight")]
+        public double Weight;
+
+        [BsonElement("Formula")]
+        public string Formula;
+
+        public static void InsertItems()
+        {
+            var item = new Item();
+
+            item = new Item()
+            {
+                Id = 1,
+                Name = "Line and Hook",
+                NT = 0,
+                Description = "Basic equipment for the Fishing skill. Requires a fishing rod.",
+                Cost = 50,
+                Weight = 0,
+                Formula = ""
+            };
+            new ItemDataAccess().InsertOne(item);
+
+            item = new Item()
+            {
+                Id = 2,
+                Name = "Isqueiro",
+                NT = 6,
+                Description = "Starts a fire",
+                Cost = 10,
+                Weight = 0,
+                Formula = ""
+            };
+            new ItemDataAccess().InsertOne(item);
+
+            item = new Item()
+            {
+                Id = 3,
+                Name = "Laser dot",
+                NT = 8,
+                Description = "Laser aim for guns.",
+                Cost = 15,
+                Weight = 0,
+                Formula = "shooting_skill:+1"
+            };
+
+            item = new Item()
+            {
+                Id = 4,
+                Name = "2m Rod",
+                NT = 0,
+                Description = "To fixate tents, to use as a fishing rod or as a piercing tool.",
+                Cost = 5,
+                Weight = 1.5,
+                Formula = ""
+            };
+            new ItemDataAccess().InsertOne(item);
+        }
+    }
+}
