07269b73e4cf39070e4ebee782a6eb875287b068
André Soares <770688@tce.ro.gov.br>
2018-09-19 08:52:19 -0400

    Admin functions

---
+++ gurps-manager-api/Controllers/AdminController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+
+namespace gurps_manager_api.Controllers
+{
+    [Route("api/[controller]")]
+    public class AdminController : Controller
+    {
+        [HttpGet]
+        public ViewResult ListClearFunctions()
+        {
+            var classList = Assembly.GetExecutingAssembly().GetTypes()
+                 .Where(t => t.Namespace == "gurps_manager_api.Controllers")
+                 .ToList();
+            var list = new List<string>();
+            foreach (var className in classList.Where(x => !x.Name.Contains( "AdminController")))
+            {
+                list.Add(className.Name.Replace("Controller", ""));
+            }
+            list.RemoveAt(list.Count-1);
+
+            return View(list);
+        }
+    }
+}
