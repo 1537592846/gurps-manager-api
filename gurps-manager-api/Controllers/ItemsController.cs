baf022ce224ed85d81c899a8ab5f3438f3346b41
André Soares <770688@tce.ro.gov.br>
2018-10-19 11:58:34 -0400

    Added new function

--- gurps-manager-api/Controllers/ItemsController.cs
+++ gurps-manager-api/Controllers/ItemsController.cs
@@ -15,6 +15,12 @@ namespace gurps_manager_api.Controllers
             return JsonConvert.SerializeObject(new ItemDataAccess().FindAll<Item>());
         }
 
+        [HttpGet("getType/{type}")]
+        public string Get(string type)
+        {
+            return JsonConvert.SerializeObject(new ItemDataAccess().FindByType(type));
+        }
+
         [HttpGet("get/{id}")]
         public string Get(int id)
         {
--- gurps-manager-api/DataAccess/ItemDataAccess.cs
+++ gurps-manager-api/DataAccess/ItemDataAccess.cs
@@ -1,9 +1,18 @@
-﻿namespace gurps_manager_library.DataAccess
+﻿using gurps_manager_library.Models;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace gurps_manager_library.DataAccess
 {
     public class ItemDataAccess : DataAccess
     {
         public ItemDataAccess() : base("Items")
         {
+        }
+
+        public List<Item> FindByType(string type)
+        {
+            return base.FindAll<Item>().Where(x => x.Type == type).ToList();
         }
     }
 }
